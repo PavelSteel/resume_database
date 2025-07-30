3b220ebaf29ddf460118600694a281f201134685
PavelSteel <misskiisteel@gmail.com>
2025-07-30 20:21:28 +0300

    реализация методов clear() и delete()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import sun.security.util.ArrayUtil;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,6 +7,9 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length - 1; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
@@ -19,7 +24,7 @@ public class ArrayStorage {
     Resume get(String uuid) {
         Resume g = new Resume();
         for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i].toString() == uuid) {
+            if (storage[i].toString().equals(uuid)) {
                 g = storage[i];
             } else {
                 g = null;
@@ -29,7 +34,17 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-
+        for (int i = 0; i < storage.length; i++) {
+            if (i != storage.length - 1) {
+                if (storage[i].toString().equals(uuid)) {
+                    for (int j = i; j < storage.length; j++) {
+                        storage[j] = storage[j + 1];
+                    }
+                }
+            } else {
+                storage[i] = null;
+            }
+        }
     }
 
     /**
